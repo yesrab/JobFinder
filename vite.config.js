57189e2f6ff849d22ff22ab428a2b5771fdc6afa
yesrab <yesrab.ali111@gmail.com>
2024-01-30 17:08:55 +0530

    vite config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import dotenv from "dotenv/config";
+import react from "@vitejs/plugin-react";
+const Port = process.env.PORT
+// console.log(Port)
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api":`http://localhost:${Port}`
+    },
+  },
+});
+
