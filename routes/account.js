dbdb40eb73250178277f19ecc2cd0db81843ec8d
yesrab <yesrab.ali111@gmail.com>
2024-01-30 17:09:15 +0530

    added favicon

--- routes/account.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { registerAccount, login } = require("../controllers/account");
-
-router.route("/register").post(registerAccount);
-router.route("/login").post(login);
-
-module.exports = router;
