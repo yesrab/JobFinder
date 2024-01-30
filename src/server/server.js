7c6a0738b5c01ae39b8300ac6126e6febcd12bc8
yesrab <yesrab.ali111@gmail.com>
2024-01-30 17:37:29 +0530

    fixed port edgecases

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 require("express-async-errors");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
